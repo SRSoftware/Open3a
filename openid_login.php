43d4c5374c774fa8b15a46cb0323a7582efd66f4
Stephan Richter <github@keawe.de>
2015-11-03 17:42:03 +0100

    updated openid_login.php

--- openid_login.php
+++ openid_login.php
@@ -13,7 +13,7 @@ $companies = array();
 
 $companies[] = array('ah-dienste','http://ah-dienste.de/sites/ah-dienste.de/themes/ahd/images/banner_farben.png','http://ah-dienste.de');
 $companies[] = array('Autocentrum Elliger','http://auto-elliger.de/sites/auto-elliger.de/files/logo_0.png','http://auto-elliger.de/');
-$companies[] = array('keawe GbR','https://keawe.de/sites/keawe.de/files/srsoftware.de_logo.gif','http://srsoftware.de');
+$companies[] = array('keawe GbR','https://keawe.de/sites/keawe.de/files/srsoftware.de_logo.gif','http://keawe.de');
 $companies[] = array('UGN-Umwelttechnik GmbH','http://www.ugn-umwelttechnik.de/sites/ugn-umwelttechnik.de/themes/UGN_2014/logo.png','http://www.ugn-umwelttechnik.de');
 
 
