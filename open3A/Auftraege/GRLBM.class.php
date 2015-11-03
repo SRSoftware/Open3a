b344ef6c3699b89185f9d64f5f3600dd2ce236c0
Stephan Richter <github@keawe.de>
2015-11-03 19:28:12 +0100

    improved option checking

--- open3A/Auftraege/GRLBM.class.php
+++ open3A/Auftraege/GRLBM.class.php
@@ -128,7 +128,9 @@ class GRLBM extends PersistentObject implements iCloneable, iRepeatable, iDeleta
 		while($t = $aC->getNextEntry()){
 			if($t->A("PostenIsAlternative") !== null AND $t->A("PostenIsAlternative") > 0)
 				continue;
-			
+			if (stripos($t->A('name'),'option') ===false) {
+				continue;
+			}	
 			
 			$rabatt = 1;
 			if(isset($t->getA()->rabatt))
@@ -142,9 +144,7 @@ class GRLBM extends PersistentObject implements iCloneable, iRepeatable, iDeleta
 			if(!isset($gesamt_netto_array[$t->A("mwst")]))
 				$gesamt_netto_array[$t->A("mwst")] = 0;
 
-			if (stripos($t->A('name'),'option') ===false) {
-				$gesamt_netto_array[$t->A("mwst")] += $nettopreis;
-			}
+			$gesamt_netto_array[$t->A("mwst")] += $nettopreis;
 			
 			if(isset($t->getA()->steuer)){
 				if(!isset($artikelsteuern[$t->A("steuer")]))
@@ -156,11 +156,9 @@ class GRLBM extends PersistentObject implements iCloneable, iRepeatable, iDeleta
 			
 			$posten_brutto = ($t->A("isBrutto") == "1" ? $t->A("bruttopreis") * $t->A("menge") * $menge2 * $rabatt : $t->A("preis") * $t->A("menge") * $menge2 * (100 + $t->A("mwst")) / 100 * $rabatt) ;
 			
-			if (stripos($t->A('name'),'option') ===false) {
-				$ges_brutto += $posten_brutto;
+			$ges_brutto += $posten_brutto;
 
-				$ges_ek1 += $t->A("menge") * $menge2 * $t->A("EK1");
-			}
+			$ges_ek1 += $t->A("menge") * $menge2 * $t->A("EK1");
 
 			if(array_search($t->A("mwst"), $mwst) === false){
 				$mwst[] = $t->A("mwst");
